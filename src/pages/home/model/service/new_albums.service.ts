6c0e747c2c747e59347b9411b86229ba6ad41285
Doni <donibro357@gmail.com>
2024-09-02 18:16:47 +0600

    fix error

--- src/pages/home/model/service/new_albums.service.ts
+++ src/pages/home/model/service/new_albums.service.ts
@@ -1,15 +1,16 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { INewReleases } from '../types/newRel-schema';
 import { ThunkConfig } from 'src/app/providers/store/config/ThunkSchema';
+import { instance } from 'src/shared/api/instance';
 
 export const getNewAlbumsTC = createAsyncThunk<INewReleases, undefined, ThunkConfig<string>>(
     'albums/new-releases',
     async (_, thunkApi) => {
-        const {rejectWithValue, extra} = thunkApi;
+        const {rejectWithValue} = thunkApi;
         const token = localStorage.getItem('accessToken');
 
         try {
-            const data = await extra.api.get('/v1/browse/new-releases', {
+            const data = await instance.get('/v1/browse/new-releases', {
               headers: {
                 Authorization: token
               }
